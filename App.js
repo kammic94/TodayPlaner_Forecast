9e61152cad664431e64e54ed0f7576cca9a4e1e0
kammic94 <kamil.michalik.94@gmail.com>
2019-05-19 20:17:30 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
 
 import React, {Component} from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Text, Button, View, Header } from 'react-native';
 import ForecastCard from './components/ForecastCard';
 
-
-export default class App extends React.Component {
+export default class App extends React.Component  {
 
 	constructor(props){
 		super(props);
-		
 		this.state = {
 			latitude: 0,
 			longitude: 0,
 			forecast: [],
-			error:''
+      aqiData: {},
+			error:'',
+      loaded: false,
 		};
 	}
-
 	componentDidMount(){
-		// Get the user's location
 		this.getLocation();
-	}
-
+    this.getAirQ();
+  }
 	getLocation(){
-
-		// Get the current position of the user
 		navigator.geolocation.getCurrentPosition(
 			(position) => {
 				this.setState(
@@ -38,13 +34,8 @@ export default class App extends React.Component {
 			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
 		);
 	}
-
 	getWeather(){
-
-		// Construct the API url to call
 		let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=c6123982940d7b0a7f2e210ce3b0dd95';
-
-		// Call the API, and set the state of the weather forecast
 		fetch(url)
 		.then(response => response.json())
 		.then(data => {
@@ -53,10 +44,41 @@ export default class App extends React.Component {
 			}));
 		})
 	}
-
+ getAirQ() {
+    this.setState({ loaded: false });
+    fetch("https://api.airvisual.com/v2/nearest_city?key=XmM3xQQDkokdEJfLh")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          aqiData: data.data,
+          loaded: true
+        });
+        console.log(this.state.aqiData);
+      });
+  }
+  airQualityStatus(aqiVal) {
+    let answer;
+    if (aqiVal <= 60) {
+      answer = "Get outside and breathe that fresh air!";
+    } else if (aqiVal > 60 && aqiVal < 130) {
+      answer = "Take it easy, the air qaulity is less than ideal!";
+    } else if (aqiVal > 130 && aqiVal < 200) {
+      answer = "It is getting bad, stay inside!";
+    } else {
+      answer = "Don't even bother breathing...";
+    }
+    return answer;
+  }
 	render() {
 		return (
-			<FlatList data={this.state.forecast.list} style={{marginTop:20}} keyExtractor={item => item.dt_txt} renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
-		);
+    <React.Fragment>
+    <View>
+      <Text> "Jakosc powietrza:"+
+        {this.airQualityStatus(this.props.aqiData.current.pollution.aqius)}
+      </Text>
+    </View>
+     <FlatList data={this.state.forecast.list} style={{marginTop:20}} keyExtractor={item => item.dt_txt} renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} /> 
+    </React.Fragment> 
+  	);
 	}
 }
